3d8f5d31c695c4d98ba9a9f3fc73f1229f003691
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-23 19:51:46 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import app from "./app";
 dotenv.config();
-
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("Success on db");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`Server runnig on port: ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("Error on connnecting to db", err);
