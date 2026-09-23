e16720f67ff91668e264eccbdb7ab9dd525b82b5
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-23 19:33:49 +0900

    feat: connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,13 @@
 import dotenv from "dotenv";
-dotenv.config()
+import mongoose from "mongoose";
+dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("Success on db");
+  })
+  .catch((err) => {
+    console.log("Error on connnecting to db", err);
+  });
