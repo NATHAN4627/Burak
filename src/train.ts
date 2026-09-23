07112caa4ac5783b22f7cff71f0e72c6ed36cbd8
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-23 19:14:57 +0900

    M Task complete

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,24 @@
-// L Task
-function reverseSentence(input: string): string {
-  return input
-    .split(" ")
-    .map((word) => word.split("").reverse().join(""))
-    .join(" ");
+// M Task
+interface SquareArray {
+  number: number;
+  square: number;
 }
 
-console.log(reverseSentence("MIT group"));
+function getSquareNumbers(nums: number[]): SquareArray[] {
+  return nums.map((num) => ({
+    number: num,
+    square: num * num,
+  }));
+}
+
+console.log(getSquareNumbers([1, 2, 3, 4, 5]));
+
+// L Task
+// function reverseSentence(input: string): string {
+//   return input
+//     .split(" ")
+//     .map((word) => word.split("").reverse().join(""))
+//     .join(" ");
+// }
+
+// console.log(reverseSentence("MIT group"));
