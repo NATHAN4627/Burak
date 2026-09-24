156a3b3832c08c98d7ed60ec7c1b4bebee13284d
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-25 00:06:11 +0900

    N Task Complete

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 import app from "./app";
-dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL as string, {})
+  .connect(process.env.MONGO_URL as string)
   .then((data) => {
     console.log("Success on db");
     const PORT = process.env.PORT ?? 3003;
@@ -15,3 +15,7 @@ mongoose
   .catch((err) => {
     console.log("Error on connnecting to db", err);
   });
+
+
+  // node js ni 22.0 versionga utkazamiz birinchi 
+  // mongoose schema yaratishga ham yani table lar yaratishga ham yordam beradi
--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,29 @@
+// N Task
+
+function palindromCheck(input: string): boolean {
+  const reverseInput = input.toLowerCase().split("").reverse().join("");
+  if (input.toLowerCase() === reverseInput) {
+    return true;
+  }
+  return false;
+}
+
+console.log(palindromCheck("abbA"));
+
 // M Task
-interface SquareArray {
-  number: number;
-  square: number;
-}
+// interface SquareArray {
+//   number: number;
+//   square: number;
+// }
 
-function getSquareNumbers(nums: number[]): SquareArray[] {
-  return nums.map((num) => ({
-    number: num,
-    square: num * num,
-  }));
-}
+// function getSquareNumbers(nums: number[]): SquareArray[] {
+//   return nums.map((num) => ({
+//     number: num,
+//     square: num * num,
+//   }));
+// }
 
-console.log(getSquareNumbers([1, 2, 3, 4, 5]));
+// console.log(getSquareNumbers([1, 2, 3, 4, 5]));
 
 // L Task
 // function reverseSentence(input: string): string {
